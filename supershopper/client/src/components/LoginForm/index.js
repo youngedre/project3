904436862239de36316cc6107ed035fb58abd7ab
William Whitted <youngedre@gmail.com>
2019-11-19 21:07:54 -0500

    fixed background issues with user homepage caused by login and signup changes

--- supershopper/client/src/components/LoginForm/index.js
+++ supershopper/client/src/components/LoginForm/index.js
@@ -40,7 +40,7 @@ class LoginForm extends Component {
   };
   render(){
     return(
-      <div className='container'>
+      <div id='background'>
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
       <Header as='h2' color='red' textAlign='center'>
